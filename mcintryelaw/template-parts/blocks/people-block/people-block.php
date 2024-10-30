74b945ab7d43490b543226b0f8a0522535575499
Doug Alexander <doug@proximodev.com>
2024-10-29 17:36:23 -0700

    Add blocks

---
+++ mcintryelaw/template-parts/blocks/people-block/people-block.php
@@ -0,0 +1,58 @@
+<?php
+/**
+* Block Name: People Block
+*/
+?>
+
+<section class="sl-attorneys">
+  <h2 class="sl-attorneys__title"> Our Attorneys
+  </h2>
+  <p class="sl-attorneys__text">We believe in one on one interaction with our clients and keeping them fully informed with the legal process. As communication is critical, we spend an extensive amount of time with our clients to ensure they understand every aspect of their case.
+  </p>
+  <div class="sl-attorneys__inner">
+    <div class="sl-attorneys__prev">
+      <svg class="svg svg-b-angle-rl" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-rl"></use>
+      </svg>
+    </div>
+    <div class="sl-attorneys__list">
+      <div class="sl-attorneys__item"><img class="sl-attorneys__img" src="assets/images/slides/atty-1.png" alt="logo image"/><a href="#">
+          <h5 class="sl-attorneys__item-title">Noble McIntyre
+          </h5></a>
+      </div>
+      <div class="sl-attorneys__item"><img class="sl-attorneys__img" src="assets/images/slides/atty-2.png" alt="logo image"/><a href="#">
+          <h5 class="sl-attorneys__item-title">Jeremy Thurman
+          </h5></a>
+      </div>
+      <div class="sl-attorneys__item"><img class="sl-attorneys__img" src="assets/images/slides/atty-3.png" alt="logo image"/><a href="#">
+          <h5 class="sl-attorneys__item-title">Jordan Klingler
+          </h5></a>
+      </div>
+      <div class="sl-attorneys__item"><img class="sl-attorneys__img" src="assets/images/slides/atty-4.png" alt="logo image"/><a href="#">
+          <h5 class="sl-attorneys__item-title">Monica Schweighart
+          </h5></a>
+      </div>
+      <div class="sl-attorneys__item"><img class="sl-attorneys__img" src="assets/images/slides/atty-1.png" alt="logo image"/><a href="#">
+          <h5 class="sl-attorneys__item-title">Noble McIntyre
+          </h5></a>
+      </div>
+      <div class="sl-attorneys__item"><img class="sl-attorneys__img" src="assets/images/slides/atty-2.png" alt="logo image"/><a href="#">
+          <h5 class="sl-attorneys__item-title">Jeremy Thurman
+          </h5></a>
+      </div>
+      <div class="sl-attorneys__item"><img class="sl-attorneys__img" src="assets/images/slides/atty-3.png" alt="logo image"/><a href="#">
+          <h5 class="sl-attorneys__item-title">Jordan Klingler
+          </h5></a>
+      </div>
+      <div class="sl-attorneys__item"><img class="sl-attorneys__img" src="assets/images/slides/atty-4.png" alt="logo image"/><a href="#">
+          <h5 class="sl-attorneys__item-title">Monica Schweighart
+          </h5></a>
+      </div>
+    </div>
+    <div class="sl-attorneys__next">
+      <svg class="svg svg-b-angle-rr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-rr"></use>
+      </svg>
+    </div>
+  </div><a class="gb-button b-medium" href="#">MEET OUR FULL TEAM</a>
+</section>
