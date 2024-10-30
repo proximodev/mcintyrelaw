06c2a3c108343ca7fcce67e5960a3f0483f4a3fb
Doug Alexander <doug@proximodev.com>
2024-10-29 17:33:45 -0700

    add content tile

---
+++ mcintryelaw/template-parts/blocks/content-tile/content-tile.php
@@ -0,0 +1,88 @@
+<?php
+/**
+* Block Name: Content Tile
+*/
+?>
+
+<section class="s-tiles">
+  <div class="s-tiles__top">
+    <h2 class="s-tiles__title"> Our Lawyers Represent the Injured in a Wide Range of Cases
+    </h2>
+    <div class="s-tiles__btn-wrapper"><a class="gb-button b-medium" href="#">VIEW ALL PRACTICE AREAS</a>
+    </div>
+  </div>
+  <div class="s-tiles__list"><a class="s-tiles__item" href="#"><img class="s-tiles__item-img" src="assets/images/tiles/tile-7.png" alt="tile image"/>
+    <div class="s-tiles__item-hover">
+    </div>
+    <div class="s-tiles__item-info">
+      <h4 class="s-tiles__item-title">Car <br> Accidents
+      </h4>
+      <svg class="svg svg-b-angle-wr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-wr"></use>
+      </svg>
+    </div></a><a class="s-tiles__item" href="#"><img class="s-tiles__item-img" src="assets/images/tiles/tile-8.png" alt="tile image"/>
+    <div class="s-tiles__item-hover">
+    </div>
+    <div class="s-tiles__item-info">
+      <h4 class="s-tiles__item-title">Truck <br> Accidents
+      </h4>
+      <svg class="svg svg-b-angle-wr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-wr"></use>
+      </svg>
+    </div></a><a class="s-tiles__item" href="#"><img class="s-tiles__item-img" src="assets/images/tiles/tile-3.png" alt="tile image"/>
+    <div class="s-tiles__item-hover">
+    </div>
+    <div class="s-tiles__item-info">
+      <h4 class="s-tiles__item-title">Motorcycle <br> Accidents
+      </h4>
+      <svg class="svg svg-b-angle-wr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-wr"></use>
+      </svg>
+    </div></a><a class="s-tiles__item" href="#"><img class="s-tiles__item-img" src="assets/images/tiles/tile-6.png" alt="tile image"/>
+    <div class="s-tiles__item-hover">
+    </div>
+    <div class="s-tiles__item-info">
+      <h4 class="s-tiles__item-title">Bicycle <br> Accidents
+      </h4>
+      <svg class="svg svg-b-angle-wr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-wr"></use>
+      </svg>
+    </div></a><a class="s-tiles__item" href="#"><img class="s-tiles__item-img" src="assets/images/tiles/tile-2.png" alt="tile image"/>
+    <div class="s-tiles__item-hover">
+    </div>
+    <div class="s-tiles__item-info">
+      <h4 class="s-tiles__item-title">Dog Bites
+      </h4>
+      <svg class="svg svg-b-angle-wr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-wr"></use>
+      </svg>
+    </div></a><a class="s-tiles__item" href="#"><img class="s-tiles__item-img" src="assets/images/tiles/tile-1.png" alt="tile image"/>
+    <div class="s-tiles__item-hover">
+    </div>
+    <div class="s-tiles__item-info">
+      <h4 class="s-tiles__item-title">Slip and <br> Fall
+      </h4>
+      <svg class="svg svg-b-angle-wr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-wr"></use>
+      </svg>
+    </div></a><a class="s-tiles__item" href="#"><img class="s-tiles__item-img" src="assets/images/tiles/tile-4.png" alt="tile image"/>
+    <div class="s-tiles__item-hover">
+    </div>
+    <div class="s-tiles__item-info">
+      <h4 class="s-tiles__item-title">Swimming Pool Accidents
+      </h4>
+      <svg class="svg svg-b-angle-wr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-wr"></use>
+      </svg>
+    </div></a><a class="s-tiles__item" href="#"><img class="s-tiles__item-img" src="assets/images/tiles/tile-5.png" alt="tile image"/>
+    <div class="s-tiles__item-hover">
+    </div>
+    <div class="s-tiles__item-info">
+      <h4 class="s-tiles__item-title">Bus <br> Accidents
+      </h4>
+      <svg class="svg svg-b-angle-wr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <use xlink:href="assets/images/_set.svg#b-angle-wr"></use>
+      </svg>
+    </div></a>
+  </div>
+</section>
