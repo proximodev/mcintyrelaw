803408ee4193a228ee136ab6ea840b6e6a33005a
doug <dalexander@ispot.com>
2024-11-03 08:04:39 -0800

    Add blocks

--- mcintryelaw/inc/acf-blocks.php
+++ mcintryelaw/inc/acf-blocks.php
@@ -22,13 +22,49 @@ function my_acf_init() {
             //'enqueue_style'     => get_stylesheet_directory_uri . '/template-parts/blocks/media-block/media-block.css',
         ));
 
+        acf_register_block(array(
+            'name'              => 'people-block',
+            'title'             => __('People'),
+            'description'       => __('Tile linked to pages and posts'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'content',
+            'icon'              => 'groups',
+            'keywords'          => array( 'people', 'team', 'lawyer', 'bio'),
+            'post_types'        => array('post', 'page'),
+            'mode'              => 'edit',
+        ));
+
+        acf_register_block(array(
+            'name'              => 'gallery-block',
+            'title'             => __('Gallery'),
+            'description'       => __('Gallery compoment for photos'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'media',
+            'icon'              => 'images-alt',
+            'keywords'          => array( 'people', 'team', 'lawyer', 'bio'),
+            'post_types'        => array('post', 'page'),
+            'mode'              => 'edit',
+        ));
+
+        acf_register_block(array(
+            'name'              => 'content-tile',
+            'title'             => __('Content Tile'),
+            'description'       => __('Tile linked to pages and posts'),
+            'render_callback'   => 'my_acf_block_render_callback',
+            'category'          => 'content',
+            'icon'              => 'format-image',
+            'keywords'          => array( 'tiles', 'content'),
+            'post_types'        => array('post', 'page'),
+            'mode'              => 'edit',
+        ));
+
     }
 }
 
 function my_acf_block_render_callback( $block ) {
     $slug = str_replace('acf/', '', $block['name']);
-    if( file_exists( get_theme_file_path("/template-parts/blocks/{$slug}.php") ) ) {
-        include( get_theme_file_path("/template-parts/blocks/{$slug}.php") );
+    if( file_exists( get_theme_file_path("/template-parts/blocks/{$slug}/{$slug}.php") ) ) {
+        include( get_theme_file_path("/template-parts/blocks/{$slug}/{$slug}.php") );
     }
 }
 ?>
