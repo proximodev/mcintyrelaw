57c9c0da280d9405d68567e4b21044c3aad4d281
dodgladot <dodgladot@gmail.com>
2024-11-21 11:03:12 +0300

    modal, search updates

--- mcintryelaw/front/assets/comp-wp/forms/search-form.js
+++ mcintryelaw/front/assets/comp-wp/forms/search-form.js
@@ -1,27 +1,27 @@
 const Search = (() => {
   function init($document, $search) {
-    const elHide = $el => {
+    const hideForm = $el => {
       $el.removeClass('_search-open').addClass('_search-closed').find('input').val('');
     }
 
-    elHide($search.parent());
-    
+    hideForm($search.parent());
+
     $document
       .on('click', '._search-closed .search-toggle', function() {
         $(this).parent().addClass('_search-open').removeClass('_search-closed').find('input').focus();
       })
       .on('click', '._search-open .search-toggle', function() {
-        elHide($(this).parent());
+        hideForm($(this).parent());
       })
       .on('keyup', function (e) {
         if (e.keyCode === 27) {
-          elHide($search.parent());
+          hideForm($search.parent());
         }
     });
 
     $(window)
       .on('scroll', function() {
-        elHide($search.parent());
+        hideForm($search.parent());
       })
   }
 
