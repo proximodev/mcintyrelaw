65bea0530ed9ba3b02ac9965204d792b13605902
dodgladot <dodgladot@gmail.com>
2025-01-30 14:15:50 +0300

    updated s-videos logic and styles

--- mcintryelaw/front/assets/components/sections/s-videos/s-videos.js
+++ mcintryelaw/front/assets/components/sections/s-videos/s-videos.js
@@ -1,39 +1,47 @@
 const SVideos = (() => {
   function init($document, $sVideos) { 
-    const getIframe = src => `<iframe class="_abs" src=${src} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>`
+    $sVideos.each((i, item) => {
+      const $section = $(item);
 
-    $document
-      .on('click', '.js-more', function() {
-        const $parent =  $(this).closest('.s-videos').addClass('_opened');
+      if(device.mobile())  {
+        const getIframe = src => `<iframe class="_abs" src=${src} frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>`
         
-        $parent.find('.s-videos__list-wrapper').css('height', 'auto');
-        $(this).removeClass('js-more').addClass('js-less').text('View less');
-      })
-      .on('click', '.js-less', function() {
-        const $parent =  $(this).closest('.s-videos').removeClass('_opened');
-        const $height = $parent.find('.s-videos__list').height();
-        const $calcHeight = window.innerWidth > 600 ? '560px' : $height > 1500 ? '1320px' : 'auto';
-
-        $parent.find('.s-videos__list-wrapper').css('height', $calcHeight);
-        $(this).removeClass('js-less').addClass('js-more').text('View more');
-      })
-
-    if(device.mobile() || window.innerWidth <= 600)  {
-      const $height = $sVideos.find('.s-videos__list').height();
-      const $calcHeight = window.innerWidth > 600 ? '560px' : $height > 1500 ? '1320px' : 'auto';
-      $sVideos.find('.s-videos__list-wrapper').css('height', $calcHeight);
-
-      $sVideos.find('.s-media__inner').each((i, item) => {
-        const $item = $(item).addClass('_mobile-hide');
-        $item.prepend(getIframe($item.attr('data-yt-src')));
-        $item.removeAttr('data-open-modal').removeAttr('onclick');
-      })
-    }
-
-    if($sVideos.find('.s-videos__item').length < 4) {
-      $sVideos.find('.videos__btn-wrapper').css('display', 'none');
-      $sVideos.find('.s-videos__list-wrapper').css('height', 'auto');
-    }
+        $section.find('.s-media__inner').each((i, item) => {
+          const $item = $(item).addClass('_mobile-hide');
+          $item.prepend(getIframe($item.attr('data-yt-src')));
+          $item.removeAttr('data-open-modal').removeAttr('onclick');
+        })
+      }
+      const $videoWrapper = $section.find('.s-videos__list-wrapper');
+  
+      if($section.find('.s-videos__item').length < 4 || $videoWrapper.hasClass('view-all')) {
+        $section.find('.videos__btn-wrapper').css('display', 'none');
+        $videoWrapper.css('height', 'auto');
+      } else {
+  
+        if (device.mobile() || window.innerWidth <= 600)  {
+          const $height = $section.find('.s-videos__list').height();
+          const $calcHeight = window.innerWidth > 600 ? '560px' : $height > 1500 ? '1320px' : 'auto';
+          $videoWrapper.css('height', $calcHeight);
+        }   
+  
+        $document
+          .on('click', '.js-more', function() {
+            const $parent =  $(this).closest('.s-videos').addClass('_opened');
+            
+            $parent.find('.s-videos__list-wrapper').css('height', 'auto');
+            $(this).removeClass('js-more').addClass('js-less').text('View less');
+          })
+          .on('click', '.js-less', function() {
+            const $parent =  $(this).closest('.s-videos').removeClass('_opened');
+            const $height = $parent.find('.s-videos__list').height();
+            const $calcHeight = window.innerWidth > 600 ? '560px' : $height > 1500 ? '1320px' : 'auto';
+  
+            $parent.find('.s-videos__list-wrapper').css('height', $calcHeight);
+            $(this).removeClass('js-less').addClass('js-more').text('View more');
+          })
+      }
+    })
   }
 
   return {
