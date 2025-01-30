3c14d6ae3bf0bf95d4e29a1b9a7d73b0155c4980
dodgladot <dodgladot@gmail.com>
2025-01-30 11:00:45 +0300

    updated video list

--- mcintryelaw/front/assets/components/sections/s-videos/s-videos.js
+++ mcintryelaw/front/assets/components/sections/s-videos/s-videos.js
@@ -1,26 +1,38 @@
 const SVideos = (() => {
   function init($document, $sVideos) { 
     const getIframe = src => `<iframe class="_abs" src=${src} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>`
-    
+
     $document
       .on('click', '.js-more', function() {
         const $parent =  $(this).closest('.s-videos').addClass('_opened');
-        const $height = $parent.find('.s-videos__list').height();
-        $parent.find('.s-videos__list-wrapper').css('height', $height);
+        
+        $parent.find('.s-videos__list-wrapper').css('height', 'auto');
         $(this).removeClass('js-more').addClass('js-less').text('View less');
       })
       .on('click', '.js-less', function() {
         const $parent =  $(this).closest('.s-videos').removeClass('_opened');
-        $parent.find('.s-videos__list-wrapper').css('height', '560px');
+        const $height = $parent.find('.s-videos__list').height();
+        const $calcHeight = window.innerWidth > 600 ? '560px' : $height > 1500 ? '1320px' : 'auto';
+
+        $parent.find('.s-videos__list-wrapper').css('height', $calcHeight);
         $(this).removeClass('js-less').addClass('js-more').text('View more');
       })
 
-    if(device.mobile())  {
+    if(device.mobile() || window.innerWidth <= 600)  {
+      const $height = $sVideos.find('.s-videos__list').height();
+      const $calcHeight = window.innerWidth > 600 ? '560px' : $height > 1500 ? '1320px' : 'auto';
+      $sVideos.find('.s-videos__list-wrapper').css('height', $calcHeight);
+
       $sVideos.find('.s-media__inner').each((i, item) => {
         const $item = $(item).addClass('_mobile-hide');
         $item.prepend(getIframe($item.attr('data-yt-src')));
         $item.removeAttr('data-open-modal').removeAttr('onclick');
       })
+    }
+
+    if($sVideos.find('.s-videos__item').length < 4) {
+      $sVideos.find('.videos__btn-wrapper').css('display', 'none');
+      $sVideos.find('.s-videos__list-wrapper').css('height', 'auto');
     }
   }
 
