3f7a347124caf1dbdca7f56eb699858c800b5442
Doug Alexander <doug@proximodev.com>
2025-02-04 20:09:06 -0800

    Gallery updates

--- mcintryelaw/blocks/gallery-slider/gallery-slider.php
+++ mcintryelaw/blocks/gallery-slider/gallery-slider.php
@@ -2,45 +2,57 @@
 /**
 * Block Name: Gallery Block
 */
+include_once( get_theme_file_path("/inc/functions/get-image-properties.php") );
+
+$themeURI = get_stylesheet_directory_uri();
+$galleryHeader = get_field( 'gallery_header');
+$galleryDescription = get_field( 'gallery_description');
+$galleryImages = get_field('gallery_images');
+
 ?>
 
-<?= get_stylesheet_directory_uri() ?>
-
-<section class="sl-gallery">
+<section class="sl-gallery is-style-full-width">
   <div class="sl-gallery__wrapper">
-    <h2 class="sl-gallery__title"> Gallery
-    </h2>
-    <p class="sl-gallery__text">
-      With the help of trial lawyers all around the state, McIntyre Law has been able to reach out to the community and give them the means to cook a Thanksgiving dinner.<br>
-<br> Many different nonprofits and community members have come out to this event year after year. The University of Oklahoma's Sooner Schooner along with ponies Boomer and Sooner, OU men's basketball coach Lon Kruger, RUF/NEKS, and cheerleaders, have come out to support the event. This is the only non-OU-sponsored event that the ponies come to!
-    </p>
+    <?php if($galleryHeader): ?>
+    <h2 class="sl-gallery__title"><?= $galleryHeader; ?></h2>
+    <?php endif; ?>
+    <?php if($galleryDescription): ?>
+    <p class="sl-gallery__text"><?= $galleryDescription; ?></p>
+    <?php endif; ?>
+
     <div class="sl-gallery__inner">
       <div class="sl-gallery__prev">
         <svg class="svg svg-b-angle-rl" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          <use xlink:href="assets/images/_set.svg#b-angle-rl"></use>
+          <use xlink:href="<?= $themeURI; ?>/assets/images/_set.svg#b-angle-rl"></use>
         </svg>
       </div>
+
+      <?php if( $galleryImages ): ?>
       <div class="sl-gallery__list">
-        <div class="sl-gallery__item">
-          <div class="sl-gallery__item-inner"><img class="sl-gallery__img" src="assets/images/slides/gallery-1.png" alt="logo image"/>
-          </div>
-        </div>
-        <div class="sl-gallery__item">
-          <div class="sl-gallery__item-inner"><img class="sl-gallery__img" src="assets/images/slides/gallery-2.png" alt="logo image"/>
-          </div>
-        </div>
-        <div class="sl-gallery__item">
-          <div class="sl-gallery__item-inner"><img class="sl-gallery__img" src="assets/images/slides/gallery-1.png" alt="logo image"/>
-          </div>
-        </div>
-        <div class="sl-gallery__item">
-          <div class="sl-gallery__item-inner"><img class="sl-gallery__img" src="assets/images/slides/gallery-2.png" alt="logo image"/>
-          </div>
-        </div>
+
+        <?php foreach( $galleryImages as $galleryImage ):
+
+            $galleryImageAttributes = get_image_properties($galleryImage, "full-width");
+            $galleryImageSrc = isset($galleryImageAttributes['src']) ? esc_url($galleryImageAttributes['src']) : '';
+            $galleryImageAlt = isset($galleryImageAttributes['alt']) ? esc_attr($galleryImageAttributes['alt']) : '';
+            $galleryImageTitle = isset($galleryImageAttributes['title']) ? esc_attr($galleryImageAttributes['title']) : '';
+            ?>
+
+            <div class="sl-gallery__item">
+              <div class="sl-gallery__item-inner">
+                <img class="sl-gallery__img" src="<?= $galleryImageSrc; ?>" alt="<?= $galleryImageAlt; ?>" title="<?= $galleryImageTitle; ?>"/>
+              </div>
+            </div>
+
+        <?php endforeach; ?>
       </div>
+      <?php else: ?>
+          <p>No images are assigned to this gallery.</p>
+      <?php endif; ?>
+
       <div class="sl-gallery__next">
         <svg class="svg svg-b-angle-rr" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          <use xlink:href="assets/images/_set.svg#b-angle-rr"></use>
+          <use xlink:href="<?= $themeURI; ?>/assets/images/_set.svg#b-angle-rr"></use>
         </svg>
       </div>
     </div>
