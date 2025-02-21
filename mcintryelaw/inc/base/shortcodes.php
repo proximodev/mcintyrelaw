aa69136c4b278bac999d462fd252b27461eed7e1
doug <doug@4all.digita>
2025-02-20 23:21:39 -0800

    Added shortcode

--- mcintryelaw/inc/base/shortcodes.php
+++ mcintryelaw/inc/base/shortcodes.php
@@ -3,4 +3,10 @@
 * Custom Shortcodes
 */
 
-?>
+function display_published_date_shortcode() {
+    if (is_single() || is_page()) {
+        return '<div class="published-date">' . get_the_date() . '</div>';
+    }
+    return '';
+}
+add_shortcode('published_date', 'display_published_date_shortcode');
