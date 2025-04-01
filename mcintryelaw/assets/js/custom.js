c0ffb61b4fd0380d6dbfaf4bc96f82d0b8b85d8e
doug <doug@4all.digita>
2025-04-01 16:04:44 -0700

    Build js updates

--- mcintryelaw/assets/js/custom.js
+++ mcintryelaw/assets/js/custom.js
@@ -2,5 +2,13 @@ const $ = jQuery;
 const $document = $(document);
 
 $document.ready(function() {
-    
+
+    $('#map-section .map-overlay').on('click', function() {
+        $(this).hide(); // Hide overlay when clicked to allow interaction
+    });
+
+    $('#map-section .map-container').on('mouseleave', function() {
+        $('#map-overlay').show(); // Restore overlay when mouse leaves
+    });
+
 });
